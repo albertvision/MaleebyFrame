7fe8e3c0784b68d530847fd45efe2f912fa52565
Yasen Georgiev <avbincco@gmail.com>
2013-11-23 10:05:37 +0200

    Added XML support and doesExist() method
    Added XML support and doesExist() method

--- Display.php
+++ Display.php
@@ -12,6 +12,7 @@ namespace Maleeby;
  * @package Core
  */
 class Display extends Loader {
+
     /**
      * Display JSON data
      * @param array $args
@@ -19,17 +20,49 @@ class Display extends Loader {
      * @throws \Exception
      */
     public static function json($data, $returnString = false) {
-        if(is_array($data)) {
+        if (is_array($data)) {
             $json = json_encode($data);
         } else {
             throw new \Exception('Invalid array provided', 500);
         }
-        
-        if(!$returnString) {
+
+        if (!$returnString) {
             header('Content-type: text/json');
             echo $json;
         } else {
             return $json;
         }
     }
+
+    public static function xml($data, $returnString = false) {
+        if (is_array($data)) {
+            /**
+             * Converts array to xml
+             */
+            $xml = new \SimpleXMLElement('<api/>');
+            foreach ($data as $key => $value) {
+                if (is_array($value)) {
+                    array2xml($value, $xml->addChild($key));
+                } else {
+                    $xml->addChild($key, $value);
+                }
+            }
+
+            $xml_data = $xml->asXML();
+        } else {
+            throw new \Exception('Invalid array provided', 500);
+        }
+
+        if (!$returnString) {
+            header('Content-type: text/xml');
+            echo $xml_data;
+        } else {
+            return $xml_data;
+        }
+    }
+
+    public static function doesExist($type) {
+        return method_exists(__CLASS__, $type);
+    }
+
 }
